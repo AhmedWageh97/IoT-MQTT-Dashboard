b05762c3b04a103b9b632be12a650073cdd40fd1
Ahmed Wageh <ahmedwageh463@gmail.com>
2021-08-15 18:04:38 +0200

    Update FirebaseUpdater.py

--- FirebaseUpdater.py
+++ FirebaseUpdater.py
@@ -3,26 +3,20 @@ import paho.mqtt.subscribe as subscribe
 import time
 from firebase import firebase
 import json
-from firebase.wrapper import Firebase
+
 
 def on_message(client, userdata, message):
-	print("Received", end = ": ")
 	received_message = str(message.payload.decode("utf-8"))
-	#print(str(message.payload) + " " + message.topic)
-	print(received_message, end = " - ")
-	print(message.topic)
 	if message.topic == "Temperature_Celisus":
 		data =  { 'Temperature': received_message}
-		print("Before")
-		FirebaseClient.post("/Temperature", data)
-		print("Done")
+		firebase.post("/Temperature", data)
 	if message.topic == "location":
 		step1 = received_message.split(',')
 		longitude = step1[0][2:-1]
 		latitude = step1[1][2:-2]
 		data =  { 'Longitude': longitude,
 		'Latitude': latitude}
-		FirebaseClient.post("/Location", data)
+		firebase.post("/Location", data)
 	if message.topic == "city_data":
 		city_data = received_message.split('_')
 		
@@ -34,26 +28,23 @@ def on_message(client, userdata, message):
 		'region': region,
 		'country': country
 		}
-		FirebaseClient.post("/City Data", city_data)
+		firebase.post("/City Data", city_data)
 	if message.topic == "Humadity_Updater":
 		data =  { 'Humadity': received_message}
-		FirebaseClient.post("/Humadity", data)
+		firebase.post("/Humadity", data)
 
-#host = "mqtt.flespi.io"
-#client = mqtt.Client("FireBaseUpdater")
-#client.on_message = on_message
-#client.username_pw_set(username = "bAsRJ3lPfPLXPOe23hGdITYUthyZxPnzfFgMwrrB42pJFrzyx4B3NnO6saTQP6fS")
-#client.connect(host, port = 1883)
-#client.loop_start()
-#client.subscribe("Temperature_Celisus")
-#client.subscribe("Humadity_Updater")
-#client.subscribe("location")
-#client.subscribe("city_data")
-FirebaseClient = Firebase('https://iot-temperature-monitor-ef062-default-rtdb.firebaseio.com/')
+host = "mqtt.flespi.io"
+client = mqtt.Client("FireBaseUpdater")
+client.on_message = on_message
+client.username_pw_set(username = "bAsRJ3lPfPLXPOe23hGdITYUthyZxPnzfFgMwrrB42pJFrzyx4B3NnO6saTQP6fS")
+client.connect(host, port = 1883)
+client.loop_start()
+client.subscribe("Temperature_Celisus")
+client.subscribe("Humadity_Updater")
+client.subscribe("location")
+client.subscribe("city_data")
+firebase = firebase.FirebaseApplication('https://iot-temperature-monitor-ef062-default-rtdb.firebaseio.com/', None)
 
 
 while True:
-	Datadd = {'Hi': 5}
-	json_object = json.dumps(Datadd, indent = 4)
-	FirebaseClient.post("/Test", json_object)
 	time.sleep(1)
